57d3064f1f624e5d63889518c2cdcb42b5c7c124
Vishal Sharma <135811673+VAIKSHA@users.noreply.github.com>
2026-04-21 06:56:37 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool identical(TreeNode* a, TreeNode* b){
+        if(!a && !b) return true;
+        if(!a || !b) return false;
+
+        return (a->val == b->val) && 
+            identical(a->left, b->left) && 
+            identical(a->right, b->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;
+        if(!subRoot) return true;
+
+        if(identical(root, subRoot)){
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+                
+    }
+//////////////////////////////////////////
+    // bool identical(TreeNode* a, TreeNode* b){
+    //     if(!a && !b) return true;
+    //     if(!a || !b) return false;
+
+    //     return (a->val == b->val) && 
+    //         identical(a->left, b->left) && 
+    //         identical(a->right, b->right);
+    // }
+    // bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    //     if(!root) return false;
+    //     if(!subRoot) return true;
+   
+    //     // If they are identical starting here, we found it!
+    //     if(identical(root, subRoot)) return true;
+
+    //     // Otherwise, check the left and right children
+    //     return isSubtree(root->left, subRoot) ||
+    //            isSubtree(root->right, subRoot);
+    // }
+};
